9ead4653f11462521ade99f02869ce8689db3aac
AJINKYA <ajinkyacodes@gmail.com>
2026-09-25 22:57:52 +0530

    Stripe integration

---
+++ src/main/java/org/example/paymentservice_aug2026/controllers/StripeWebhookController.java
@@ -0,0 +1,16 @@
+package org.example.paymentservice_aug2026.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stripeWebhook")
+public class StripeWebhookController {
+
+    @PostMapping
+    public void printStripeEvent(@RequestBody String event) {
+        System.out.println(event);
+    }
+}
---
+++ src/main/java/org/example/paymentservice_aug2026/paymentgateways/StripePaymentGateway.java
@@ -0,0 +1,67 @@
+package org.example.paymentservice_aug2026.paymentgateways;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentLink;
+import com.stripe.model.Price;
+import com.stripe.param.PaymentLinkCreateParams;
+import com.stripe.param.PriceCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class StripePaymentGateway implements IPaymentGateway {
+
+    @Value("${stripe.apiKey}")
+    private String apiKey;
+
+    @Override
+    public String getPaymentLink(Long amount, String phoneNumber, String name, String email, String orderId) {
+        try {
+            Stripe.apiKey = this.apiKey;
+            Price price = getPrice(amount);
+            PaymentLinkCreateParams params =
+                    PaymentLinkCreateParams.builder()
+                            .addLineItem(
+                                    PaymentLinkCreateParams.LineItem.builder()
+                                            .setPrice(price.getId())
+                                            .setQuantity(1L)
+                                            .build()
+                            ).setAfterCompletion(PaymentLinkCreateParams.AfterCompletion.builder()
+                                    .setType(PaymentLinkCreateParams.AfterCompletion.Type.REDIRECT)
+                                    .setRedirect(PaymentLinkCreateParams.AfterCompletion.Redirect.builder()
+                                            .setUrl("https://scaler.com")
+                                            .build())
+                                    .build())
+                            .build();
+            PaymentLink paymentLink = PaymentLink.create(params);
+            return paymentLink.getUrl();
+        }catch (StripeException exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+
+    private Price getPrice(Long amount) {
+        try {
+            PriceCreateParams params =
+                    PriceCreateParams.builder()
+                            .setCurrency("usd")
+                            .setUnitAmount(amount)
+                            .setRecurring(
+                                    PriceCreateParams.Recurring.builder()
+                                            .setInterval(PriceCreateParams.Recurring.Interval.MONTH)
+                                            .build()
+                            )
+                            .setProductData(
+                                    PriceCreateParams.ProductData.builder().setName("Gold Plan").build()
+                            )
+                            .build();
+            Price price = Price.create(params);
+            return price;
+        }catch (StripeException exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+}
